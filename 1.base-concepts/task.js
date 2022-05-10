314704872c315f73dabf2970a0fc8a29cb17c997
polinkavishinka <43956791+polinkavishinka@users.noreply.github.com>
2022-05-10 21:07:42 +0300

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,29 @@
+
+// код для задачи №1 писать здесь
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  "use strict";
+ // (ax^2+bx+c = 0 )
+  let arr = [];
+  
+       let d = Math.pow(b,2) - 4*a*c;
+       
+
+        if (d === 0){
+          arr[0] = (-b + Math.sqrt(d)) / 2*a;
+          console.log(`Корень равен ${arr[0]}`);
+      }  else if (d > 0)
+        {
+          arr[0] = (-b + Math.sqrt(d)) / 2*a;
+          arr[1] = (-b - Math.sqrt(d)) / 2*a;
+          console.log (`Корни равны: ${arr[0]}, ${arr[1]}`);
+        } else  
+        {
+          console.log('Корней нет');
+        }
+  return arr;
+};
+
+solveEquation ( 1, 6, 3);
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
