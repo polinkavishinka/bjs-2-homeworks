2673cc3c1ee48f4d73f3a627ac1b7af2247a5446
polinkavishinka <43956791+polinkavishinka@users.noreply.github.com>
2022-05-16 22:28:33 +0300

    updated task file

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,68 @@
 // Задание 1
 function getArrayParams(arr) {
   let min, max, sum, avg;
-
-  // Ваш код
+  min = Infinity;
+  max = -Infinity;
+  sum = 0;
+  avg = 0;
+  //let n = arr.length;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min ) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    sum = sum + arr[i];
+  }
+   avg = Number((sum/arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
+console.log(getArrayParams([-99, 99, 10]));
+
 
 // Задание 2
-function worker(arr) {
-  let sum;
 
+//Напишите функцию worker которая должна находить сумму элементов массива и возвращать её.
+function worker(arr) {
+  let sum = 0;
+  for ( let i = 0; i < arr.length; i++){
+    sum = sum + arr[i];
+  }
   // Ваш код
 
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
+  for ( let i = 0; i < arrOfArr.length; i++){
+       let sumArr= func(arrOfArr[i]);
 
-  // Ваш кода
-  // for ...
-  
+    if (sumArr > max){
+      max = sumArr;
+    }
+  } 
   return max;
 }
 
+
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+
+  let min = Infinity;
+  let max = -Infinity;
+  let dif;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min ) {
+      min = arr[i];
+    }
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  dif = Math.abs(min - max);
+  return dif;
 }
