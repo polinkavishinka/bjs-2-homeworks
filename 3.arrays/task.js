abaeff9ca418ba03cedf08dc44afe072ec8b1abc
polinkavishinka <43956791+polinkavishinka@users.noreply.github.com>
2022-05-19 14:39:06 +0300

    update

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  result =  arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
+  
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
+  resultArr = arr.filter((n) => n % 3 === 0 ).filter((n) => n > 0).map((n) => n*10);
+ 
 
   return resultArr; // array
 }
