0b38a1aa47250da9b437a6547872d357a0c89015
polinkavishinka <43956791+polinkavishinka@users.noreply.github.com>
2022-05-24 23:32:29 +0300

    update

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
-function Student(name, gender, age) {
-    // Ваш код
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Buzz", "female", 35);
+let student3 = new Student("Ann", "female", 30);
 
+function Student(name, gender, age) {
+  this.name = name;
+  this.gender = gender;
+  this.age = age ; 
 }
+
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) { 
+     this.marks = [mark];
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) { 
+    this.marks = [...marks];
+   } else {
+     this.marks.push(...marks);
+   }
+}
+
+Student.prototype.getAverage = function () {
+  let x = 0;
+  let res = 0;
+  for (i = 0; i < this.marks.length; i++) {
+    x += this.marks[i];
+  }
+  return res = x / this.marks.length;
+}
+
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
